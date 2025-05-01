7baca52e8a6885e1423dce496f2d31ee79065e68
don1xdev <mityafotografer@gmail.com>
2025-05-01 13:00:46 +0300

    1

---
+++ src/main/java/ru/don1x/advancedchatutility/AdvancedChatUtility.java
@@ -0,0 +1,17 @@
+package ru.don1x.advancedchatutility;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedChatUtility extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
